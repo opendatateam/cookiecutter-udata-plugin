808dffc5701e85aa8064757f210c78eebf15d000
Axel Haustant <noirbizarre@gmail.com>
2018-05-11 15:42:16 +0200

    Removes unnecessary tests

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,16 +15,27 @@ TO_REMOVE = [
     {% if cookiecutter.has_preview == 'no' %}'preview.py',{% endif %}
 ]
 
+TESTS_TO_REMOVE = [
+    {% if cookiecutter.has_harvester == 'no' %}'test_{{ cookiecutter.identifier.replace('-', '_') }}_backend.py',{% endif %}
+    {% if cookiecutter.has_views == 'no' %}'test_{{ cookiecutter.identifier.replace('-', '_') }}_views.py',{% endif %}
+    {% if cookiecutter.has_preview == 'no' %}'test_{{ cookiecutter.identifier.replace('-', '_') }}_preview.py',{% endif %}
+]
+
+
+def remove(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path, ignore_errors=True)
+    else:
+        os.remove(path)
+
 
 if __name__ == '__main__':
 
     # Remove unnecessary files
     for name in TO_REMOVE:
-        path = os.path.join(PROJECT_DIRECTORY,
-                            # '{{ cookiecutter.project_slug }}',
-                            '{{ cookiecutter.pypackage }}',
-                            name)
-        if os.path.isdir(path):
-            shutil.rmtree(path, ignore_errors=True)
-        else:
-            os.remove(path)
+        path = os.path.join(PROJECT_DIRECTORY, '{{ cookiecutter.pypackage }}', name)
+        remove(path)
+
+    for name in TESTS_TO_REMOVE:
+        path = os.path.join(PROJECT_DIRECTORY, 'tests', name)
+        remove(path)
