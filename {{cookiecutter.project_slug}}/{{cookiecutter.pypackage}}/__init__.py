294fe94ed207d6a4eecb701070fafe2fc083a81e
maudetes <maudet.estelle@gmail.com>
2022-07-13 10:51:39 +0200

    Update CircleCI

--- {{cookiecutter.project_slug}}/{{cookiecutter.pypackage}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pypackage}}/__init__.py
@@ -4,7 +4,12 @@
 {{ cookiecutter.description }}
 '''
 
-__version__ = '{{ cookiecutter.version }}'
+import os
+
+tag = os.environ.get('CIRCLE_TAG')
+build_num = os.environ.get('CIRCLE_BUILD_NUM')
+
+__version__ = '{{ cookiecutter.version }}' + (str(build_num) if not tag and build_num else '0')
 __description__ = '{{ cookiecutter.description }}'
 {% if cookiecutter.has_generic_plugin == 'yes' %}
 
